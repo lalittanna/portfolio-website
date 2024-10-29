534769e9bfefc76d5db1afea501fcf63a099608f
lalit <lalittanna77@gmail.com>
2024-10-29 11:37:31 +0000

    build fix

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,14 +1,13 @@
-const { merge } = require('webpack-merge')
-const commonConfiguration = require('./webpack.common.js')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { merge } = require('webpack-merge');
+const path = require('path');
+const commonConfiguration = require('./webpack.common.js');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = merge(
-    commonConfiguration,
-    {
-        mode: 'production',
-        plugins:
-        [
-            new CleanWebpackPlugin()
-        ]
-    }
-)
+module.exports = merge(commonConfiguration, {
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, '../build'), // specify the output directory
+        filename: '[name].[contenthash].js', // hashed filenames for cache busting
+    },
+    plugins: [new CleanWebpackPlugin()],
+});
